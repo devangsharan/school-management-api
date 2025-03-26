da88d06b928119d420a59c59bf11a327f7bb67ef
devangsharan <devangsharan2002@gmail.com>
2025-03-26 21:25:02 +0530

    Updated schoolRoutes.js

--- routes/schoolRoutes.js
+++ routes/schoolRoutes.js
@@ -1,8 +1,12 @@
-const express = require('express');
-const { addSchool, listSchools } = require('../controllers/schoolController');
+const express = require("express");
+const { addSchool, listSchools } = require("../controllers/schoolController");
+
 const router = express.Router();
 
-router.post('/addSchool', addSchool);
-router.get('/listSchools', listSchools);
+// Route to add a school
+router.post("/schools", addSchool);
+
+// Route to list all schools
+router.get("/schools", listSchools);
 
 module.exports = router;
